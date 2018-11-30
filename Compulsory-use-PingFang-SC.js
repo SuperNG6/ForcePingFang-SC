c6088bfb9abb7a009e17f9015c4c9e3e8ceb0cdf
sleele <sleele@gmail.com>
2018-11-30 16:40:27 +0800

    Update Compulsory-use-PingFang-SC.js
    update

---
+++ Compulsory-use-PingFang-SC.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         强制使用苹方
+// @namespace    
+// @version      1.0.0
+// @description  强制使用苹方字体
+// @author       NG6
+// @include      *
+// @run-at       document-start
+// @grant        unsafeWindow
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+	changeFont("PingFang SC",2);
+})();

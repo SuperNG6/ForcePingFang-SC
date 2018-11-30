d350a34af859f347fea4bf56d9ed93ae10a3b593
SuperNG6 <sleele@gmail.com>
2018-11-30 17:16:44 +0800

    Update Compulsory-use-PingFang-SC.js
    update

--- Compulsory-use-PingFang-SC.js
+++ Compulsory-use-PingFang-SC.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         强制使用苹方
-// @namespace    https://raw.githubusercontent.com/SuperNG6/Compulsory-use-PingFang-SC/master/Compulsory-use-PingFang-SC.js
-// @version      1.1.0
+// @namespace    https://github.com/SuperNG6/Compulsory-use-PingFang-SC.git
+// @version      1.3.0
 // @description  强制使用苹方字体
 // @author       NG6
 // @include      *
 // @run-at       document-start
 // @grant        unsafeWindow
 // @license      MIT
+// @require https://greasyfork.org/scripts/29500-%E5%BC%BA%E5%88%B6%E4%BD%BF%E7%94%A8%E5%AD%97%E4%BD%93-%E6%A0%B8%E5%BF%83/code/%E5%BC%BA%E5%88%B6%E4%BD%BF%E7%94%A8%E5%AD%97%E4%BD%93%20-%20%E6%A0%B8%E5%BF%83.js?version=640709
 // ==/UserScript==
 
 (function() {
